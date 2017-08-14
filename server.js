c7d30b493764ad7cb90b00c14ab9491d5f62c136
Rohithgilla12 <gillarohith1@gmail.com>
2017-08-14 20:19:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,11 +12,9 @@ app.get('/article-one',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
-   res.send('article-two is serverd here');
-});
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));});
 app.get('/article-three',function(req,res){
-   res.send('article-three is serverd here');
-});
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
