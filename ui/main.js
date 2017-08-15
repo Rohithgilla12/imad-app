8a947eb4abeb40922bf3a5244f6573df391e5da9
Rohithgilla12 <gillarohith1@gmail.com>
2017-08-15 11:21:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,3 +28,15 @@ button.onclick = function () {
 
 
 };
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit-btn');
+submit.onclick=function(){
+    var names=['name1','name2','name 3'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+= '<li>' + names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+};
