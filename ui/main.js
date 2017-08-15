6945de18e303ba1423e07c819c7090956921b4e0
Rohithgilla12 <gillarohith1@gmail.com>
2017-08-15 10:20:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,14 +11,14 @@ img.onclick=function(){
 };
 var counter=0;
 var button=document.getElementById('counter');
+
 button.onclick=function(){
-    //Req
-    
-    
-    //Response
-    
-    
-    //Render
+    var request= new XMLHttpRequest();
+    request.onreadystatechange=function(){
+      if(request.readyState===XMLHttpRequest.DONE){
+          
+      }
+    };
     counter=counter+1;
     var span=document.getElementById('count');
     span.innerHTML=counter.toString();
