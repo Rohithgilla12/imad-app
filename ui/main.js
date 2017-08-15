b8a5f18d362f4d4e133078d809f880bc3ae7c4f5
Rohith Gilla <gillarohith1@gmail.com>
2017-08-15 10:38:56 +0530

    main.js added

--- ui/main.js
+++ ui/main.js
@@ -16,11 +16,13 @@ button.onclick=function(){
     var request= new XMLHttpRequest();
     request.onreadystatechange=function(){
       if(request.readyState===XMLHttpRequest.DONE){
-          
+          if(request.status===200){
+              var counter=request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+          }
       }
     };
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+
     
 };
