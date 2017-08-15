5ae53934b40d1f918f2d10a585d8a8b1433b0004
Rohith Gilla <gillarohith1@gmail.com>
2017-08-15 10:43:33 +0530

    Added request to main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,31 @@
 console.log('Loaded!');
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
- marginLeft=marginLeft+1;
- img.style.marginLeft=marginLeft+'px';
+var img = document.getElementById('madi');
+var marginLeft = 0;
+
+function moveRight() {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
 }
-img.onclick=function(){
-  var interval=setInterval(moveRight,50);
+img.onclick = function () {
+    var interval = setInterval(moveRight, 50);
 
 };
-var counter=0;
-var button=document.getElementById('counter');
+var counter = 0;
+var button = document.getElementById('counter');
 
-button.onclick=function(){
-    var request= new XMLHttpRequest();
-    request.onreadystatechange=function(){
-      if(request.readyState===XMLHttpRequest.DONE){
-          if(request.status===200){
-              var counter=request.responseText;
-              var span=document.getElementById('count');
-              span.innerHTML=counter.toString();
-          }
-      }
+button.onclick = function () {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
     };
+    request.open('GET', 'http://gillarohith1.imad.hasura-app.io/counter', true);
+    request.send(null)
 
-    
+
 };
