fccfb416d2c67e7973d934d101805c21ac6e867e
#Gilla_Dude! <gillarohith1@gmail.com>
2017-09-09 09:35:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,28 +29,44 @@ button.onclick = function () {
 
 };
 
-var submit=document.getElementById('submit-btn');
-submit.onclick=function(){
-        var request = new XMLHttpRequest();
+var submit = document.getElementById('submit-btn');
+submit.onclick = function () {
+    var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                    var names=request.responseText;
-                    names=JSON.parse(names);
-                    var list='';
-                    for(var i=0;i<names.length;i++){
-                        list+= '<li>' + names[i]+'</li>';
-                    }
-                    var ul=document.getElementById('namelist');
-                    ul.innerHTML=list;
-                
+                //                var names = request.responseText;
+                //                names = JSON.parse(names);
+                //                var list = '';
+                //                for (var i = 0; i < names.length; i++) {
+                //                    list += '<li>' + names[i] + '</li>';
+                //                }
+                //                var ul = document.getElementById('namelist');
+                //                ul.innerHTML = list;
+                console.log('User logged in sucessfully');
+                alert('Logged in');
+
+
             }
+            if(request.status===403)
+                {
+                  alert('Check your Crendentials')  
+                }
+            if(request.status===500)
+                {
+                    alert('Server down :(')
+                }
         }
     };
-    var nameInput=document.getElementById('name');
-    var name=nameInput.value;
-    request.open('GET', 'http://gillarohith1.imad.hasura-app.io/submit-name/?name='+name, true);
-    request.send(null);
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    request.open('POST', 'http://gillarohith1.imad.hasura-app.io/login' + name, true);
+    request.send(JSON.stringify({
+        username: username,
+        password: password
+    }));
 
-   
+
 };
