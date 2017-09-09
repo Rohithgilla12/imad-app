3e5965584d9e1c19db212f5cc97d807826f99b8d
#Gilla_Dude! <gillarohith1@gmail.com>
2017-09-09 08:48:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -151,33 +151,65 @@ app.post('/create-user',function(req,res){
    });
 
 });
-app.post('/login',function(req,res){
-   var username=req.body.username;
-   var password=req.body.password;
-   var salt = crypto.randomBytes(128).toString('hex');
-   var dbString=hash(password,salt);
-   pool.query('SELECT * FROM "user" WHERE username=$1',[username],function(err,result){
-         if(err){
-           res.status(500).send(err.toString());
+// app.post('/login',function(req,res){
+//   var username=req.body.username;
+//   var password=req.body.password;
+//   var salt = crypto.randomBytes(128).toString('hex');
+//   var dbString=hash(password,salt);
+//   pool.query('SELECT * FROM "user" WHERE username=$1',[username],function(err,result){
+//          if(err){
+//           res.status(500).send(err.toString());
 
-       }
-       else{
-        if(result.rows.length===0){
-            res.status(403).send('user not found asshole');
-        }
-        else{
-            var dbString=result.rows[0].password;
-            var salt=dbString.split('$')[2];
-            var hashedPassword=hash(password,salt);
-            if(hashedPassword===dbstring){
-                res.send('Correct details dude');
-            }
-            else{
-                res.status(403).send('user not found asshole');
-            }
+//       }
+//       else{
+//         if(result.rows.length===0){
+//             res.status(403).send('user not found asshole');
+//         }
+//         else{
+//             var dbString=result.rows[0].password;
+//             var salt=dbString.split('$')[2];
+//             var hashedPassword=hash(password,salt);
+//             if(hashedPassword===dbstring){
+//                 res.send('Correct details dude');
+//             }
+//             else{
+//                 res.status(403).send('user not found asshole');
+//             }
 
-        }
-       }
+//         }
+//       }
+//   });
+// });
+app.post('/login', function (req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          if (result.rows.length === 0) {
+              res.status(403).send('username/password is invalid');
+          } else {
+              // Match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+              if (hashedPassword === dbString) {
+                
+                // Set the session
+                req.session.auth = {userId: result.rows[0].id};
+                // set cookie with a session id
+                // internally, on the server side, it maps the session id to an object
+                // { auth: {userId }}
+                
+                res.send('credentials correct!');
+                
+              } else {
+                res.status(403).send('username/password is invalid');
+              }
+          }
+      }
    });
 });
 var pool=new Pool(config);
